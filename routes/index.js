d31e575122d9dc74fcc5d74628142c140c4141d0
Rene <mail@reneschimmelpfennig.de>
2015-07-22 21:46:19 +0200

    added "header" and "footer" areas to layout view added selfhosted jQuery added AngularJS added material-icons support added "add document" button to index view optmized layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'upload',
+    angularApp: 'UploadApp'
+  });
 });
 
 router.post('/upload', function(req, res) {
