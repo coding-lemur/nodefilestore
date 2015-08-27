3781d916c4f1b573d4596fd74548567c852f631b
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-08-27 21:46:11 +0200

    added "ng-app" attribute to layout view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'upload'
+    title: 'upload',
+    angularApp: 'UploadApp'
   });
 });
 
