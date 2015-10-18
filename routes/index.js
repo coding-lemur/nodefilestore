fa219db9c4aa36e5c49a2d83729be088153d480b
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-10-18 13:46:26 +0200

    added config file added backend logic for upload (unfinished)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 var multer  = require('multer');
-var upload = multer();
+var gridfsStorage = require('gridfs-storage-engine')({ url: config.database.connection });
+var upload = multer({ storage: gridfsStorage });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,10 +13,14 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.post('/upload', upload.array('myFile'), function (req, res) {
-    console.log(req.files);
+router.post('/upload', upload.array('files'), function (req, res) {
+    var id = req.files[0].gridfsEntry._id.toJSON();
 
-    res.status(201);
+    res.status(201).json({ id: id });
+});
+
+router.get('/files/:id', function (req, res) {
+    res.status(200);
 });
 
 module.exports = router;
