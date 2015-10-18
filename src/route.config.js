33b6a503b40af28e2366375b4cbea8d519465cbb
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-10-18 18:35:09 +0200

    using HTML5 history for routing

--- src/route.config.js
+++ src/route.config.js
@@ -1,7 +1,7 @@
 import UploadController from './upload.controller';
 import InfoController from './info.controller';
 
-export default function RouteConfig($routeProvider) {
+export default function RouteConfig($routeProvider, $locationProvider) {
     $routeProvider
         .when('/upload', {
             templateUrl: '/partials/upload.html',
@@ -16,6 +16,9 @@ export default function RouteConfig($routeProvider) {
         .otherwise({
             redirectTo: '/upload'
         });
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
 }
 
-RouteConfig.$inject = ['$routeProvider'];
+RouteConfig.$inject = ['$routeProvider', '$locationProvider'];
