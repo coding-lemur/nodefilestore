f791eb20855a55db3c8e42e21bf8c4345ece16a0
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-10-18 16:45:40 +0200

    added backend logic for uploading files

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,54 @@ var config = require('config');
 var multer  = require('multer');
 var gridfsStorage = require('gridfs-storage-engine')({ url: config.database.connection });
 var upload = multer({ storage: gridfsStorage });
+var MongoClient = require('mongodb').MongoClient;
+var httpError = require('../helper/httpError');
+var uuid = require('node-uuid');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.render('index', {
         title: 'upload',
         angularApp: 'UploadApp'
     });
 });
 
-router.post('/upload', upload.array('files'), function (req, res) {
-    var id = req.files[0].gridfsEntry._id.toJSON();
+router.post('/upload', upload.array('files'), function(req, res, next) {
+    //var id = req.files[0].gridfsEntry._id.toJSON();
+    var fileIds = [req.files[0].gridfsEntry._id];
 
-    res.status(201).json({ id: id });
+    MongoClient.connect(config.database.connection, function(err, db) {
+        if (err) {
+            return next(httpError.createError(500, err));
+        }
+
+        insertUpload(db, fileIds, function(err, result, upload) {
+            if (err) {
+                return next(httpError.createError(500, err));
+            }
+
+            db.close();
+
+            res.status(201).json({ token: upload.token });
+        });
+    });
 });
 
 router.get('/files/:id', function (req, res) {
     res.status(200);
 });
+
+function insertUpload(db, fileIds, callback) {
+    var collection = db.collection('uploads');
+    var upload = {
+        token: uuid.v4(),
+        files: fileIds,
+        createDate: new Date()
+    };
+
+    collection.insertMany([upload], function(err, result) {
+        callback(err, result, upload)
+    });
+}
 
 module.exports = router;
