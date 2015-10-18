a103281e9f4f6ff9ea997cdc5a09f695766cacd3
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-10-18 16:46:27 +0200

    added filesize filter

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import angular from './../public/libs/angularjs/angular';
 import MainController from './main.controller';
 import FilePickerDirective from './FilePicker/filePicker.directive';
 import DataService from './data.service';
+import FilesizeFilter from './filesize.filter';
 
 angular
     .module('UploadApp', [])
     .controller('MainController', MainController)
     .directive('filePicker', FilePickerDirective)
-    .service('dataService', DataService);
+    .service('dataService', DataService)
+    .filter('filesize', FilesizeFilter);
