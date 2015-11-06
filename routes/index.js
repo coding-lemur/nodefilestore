8514856d84623e80e74c0117f2e29b43cf439347
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-11-06 23:18:52 +0100

    removed redundant route

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', {
-        title: 'upload',
-        angularApp: 'UploadApp'
-    });
-});
-
-// enables HTML5Mode by forwarding missing files to the index.html
+// the "*" enables HTML5Mode by forwarding missing files to the index.html
 router.get('/*', function(req, res, next) {
     res.render('index', {
         title: 'upload',
