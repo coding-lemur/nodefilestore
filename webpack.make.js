1f31d7282f0491a87d280644e9e6557e89cecc09
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2015-12-28 19:42:09 +0100

    fixed bug to run "babel" compiler without errors after update to the new version

--- webpack.make.js
+++ webpack.make.js
@@ -90,7 +90,7 @@ module.exports = function makeWebpackConfig(options) {
             // Transpile .js files using babel-loader
             // Compiles ES6 and ES7 into ES5 code
             test: /\.js$/,
-            loader: 'babel?optional[]=runtime',
+            loader: 'babel',
             exclude: [/node_modules/, /libs/]
         }]
     };
