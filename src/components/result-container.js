284abe072f460d21da21c2a5d423b12f9ceede0e
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2016-02-21 19:54:48 +0100

    fixed missing dependency to "dateformat" switched date format to momentjs formating

--- src/components/result-container.js
+++ src/components/result-container.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import dateFormat from 'dateformat';
+import moment from 'moment';
 
 export default class ResultContainer extends React.Component {
     constructor(props) {
@@ -13,7 +12,7 @@ export default class ResultContainer extends React.Component {
         return (
             <div className="result-container">
                 <p>download-url: <a href={apiResult.downloadUrl}>{apiResult.downloadUrl}</a></p>
-                <p className="download-expires">expires on {dateFormat(apiResult.expirationDate)}</p>
+                <p className="download-expires">expires on {moment(apiResult.expirationDate).format('LLL')}</p>
             </div>
         );
     }
