8a4fd26795926939a3c26966a725a894c4bc8d9d
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2016-02-21 19:25:39 +0100

    optimized cleanup script removed orphaned files on cleanup

--- clean-up/cleanup.py
+++ clean-up/cleanup.py
@@ -2,35 +2,30 @@ from pymongo import MongoClient
 import gridfs
 import datetime
 
-
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f %s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f %s%s" % (num, 'Yi', suffix)
-
-
 today = datetime.datetime.today()
 
 client = MongoClient("mongodb://localhost:27017/nodefilestore")
 db = client.nodefilestore
+fs = gridfs.GridFS(db)
 
-expiredFileIDs = []
 expiredUploadIDs = []
+expiredFileIDs = []
+usedFileIDs = []
+totalFilesDeleted = 0
 
 # collect expired uploads
-uploads = db.uploads.find({"expirationDate": {"$lt": today}})
+uploads = db.uploads.find()
 for upload in uploads:
-    print(upload["expirationDate"])
+    if upload["expirationDate"] < today:  # expired
+        expiredUploadIDs.append(upload["_id"])
+        expiredFileIDs.extend(upload["files"])
+    else:
+        usedFileIDs.extend(upload["files"])
 
-    expiredUploadIDs.append(upload["_id"])
-    expiredFileIDs.extend(upload["files"])
-
-if len(expiredFileIDs) == 0:
+if len(expiredUploadIDs) == 0:
     print("no expired uploads")
 else:
-    print("%s expired uploads" % len(expiredUploadIDs))
+    print("%s expired upload(s)" % len(expiredUploadIDs))
 
     # delete expired uploads (metadata)
     result = db.uploads.delete_many({"_id": {"$in": expiredUploadIDs}})
@@ -38,23 +33,22 @@ else:
     if result.deleted_count == 0:
         print("no uploads deleted")
     else:
-        print("deleted %s uploads" % result.deleted_count)
+        print("deleted %s upload(s)" % result.deleted_count)
 
-    # remove files (from GridFS)
-    fs = gridfs.GridFS(db)
-
-    totalFilesDeleted = 0
-    totalStorage = 0
-
+    # remove expired files (from GridFS)
     for fileID in expiredFileIDs:
-        file = fs.find_one({"_id": fileID})
+        result = fs.delete(fileID)
+        totalFilesDeleted += 1
 
-        if file is not None:
-            totalFilesDeleted += 1
-            totalStorage += file.length
-            fs.delete(fileID)
 
-    if totalFilesDeleted == 0:
-        print("no files removed")
-    else:
-        print("files removed: %s (%s)" % (totalFilesDeleted, sizeof_fmt(totalStorage)))
+# remove orphaned files
+orphanedFiles = fs.find({"_id": {"$nin": usedFileIDs}})
+
+for file in orphanedFiles:
+    fs.delete(file._id)
+    totalFilesDeleted += 1
+
+if totalFilesDeleted == 0:
+    print("no files removed")
+else:
+    print("files removed: %s" % totalFilesDeleted)
