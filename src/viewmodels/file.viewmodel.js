3f6013aa6c5206810b9b56290d23964d66195668
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2016-03-04 19:11:26 +0100

    fixed constructor for empty files

--- src/viewmodels/file.viewmodel.js
+++ src/viewmodels/file.viewmodel.js
@@ -1,14 +1,12 @@
 export default class FileViewModel {
     constructor(file) {
-        if (!file) {
-            return;
+        if (file) {
+            this.name = file.name;
+            this.size = file.size;
+            this.mimeType = file.type;
+            this.date = file.lastModifiedDate;
+            this.originalFile = file;
         }
-
-        this.name = file.name;
-        this.size = file.size;
-        this.mimeType = file.type;
-        this.date = file.lastModifiedDate;
-        this.originalFile = file;
 
         this.uploadedPercentage = 0;
         this.isUploading = false;
