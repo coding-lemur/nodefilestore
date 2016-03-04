dc0d272578e753e88315679f6a91c6bd7fc892c5
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2016-03-04 19:11:31 +0100

    added tests

---
+++ src/components/__tests__/file.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+
+import File from '../file';
+import Progress from '../progress';
+import FileViewModel from '../../viewmodels/file.viewmodel';
+
+describe('file component', () => {
+    it('should render file', () => {
+        const file = new FileViewModel();
+        file.name = 'test.jpg';
+        file.size = 1024;
+        file.mimeType = 'image/jpeg';
+        file.date = new Date();
+
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<File file={file} />);
+
+        let actualElement = renderer.getRenderOutput();
+        let expectedElement = (<li className="collection-item file">
+            <div className="file-name">test.jpg</div>
+            <div className="file-size">1.00 Kb</div>
+        </li>);
+        expect(actualElement).toEqualJSX(expectedElement);
+    });
+
+    it('should render file and delete button', () => {
+        const file = new FileViewModel();
+        file.name = 'test.jpg';
+        file.size = 1024;
+        file.mimeType = 'image/jpeg';
+        file.date = new Date();
+
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<File file={file} showDeleteButton="true" />);
+
+        const noRefCheck = () => {};
+
+        let actualElement = renderer.getRenderOutput();
+        let expectedElement = (<li className="collection-item file">
+            <div className="file-name">test.jpg</div>
+            <div className="file-size">1.00 Kb</div>
+            <a className="remove-button" onClick={noRefCheck}>
+                <i className="material-icons">delete</i>
+            </a>
+        </li>);
+        expect(actualElement).toEqualJSX(expectedElement);
+    });
+
+    it('should render file with progress bar while uploading', () => {
+        const file = new FileViewModel();
+        file.name = 'test.jpg';
+        file.size = 1024;
+        file.mimeType = 'image/jpeg';
+        file.date = new Date();
+        file.isUploading = true;
+        file.uploadedPercentage = 17;
+
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<File file={file} />);
+
+        let actualElement = renderer.getRenderOutput();
+        let expectedElement = (<li className="collection-item file">
+            <div className="file-name">test.jpg</div>
+            <div className="file-size">1.00 Kb</div>
+            <Progress value={17} />
+        </li>);
+        expect(actualElement).toEqualJSX(expectedElement);
+    });
+});
---
+++ src/helper/__tests__/file-size-formatter.test.js
@@ -0,0 +1,53 @@
+import expect from 'expect';
+
+import FileSizeFormatter from '../file-size-formatter'
+
+describe('FileSizeFormater', () => {
+    it('should handle missing input', () => {
+        expect(FileSizeFormatter.humanize()).toBe('0 Bytes');
+    });
+
+    it('should format 1 bytes', () => {
+        expect(FileSizeFormatter.humanize(1)).toBe('1 Bytes');
+    });
+
+    it('should format 1023 bytes', () => {
+        expect(FileSizeFormatter.humanize(1023)).toBe('1023 Bytes');
+    });
+
+    it('should format 1 KB', () => {
+        expect(FileSizeFormatter.humanize(1024)).toBe('1.00 Kb');
+    });
+
+    it('should format 1023 KB', () => {
+        expect(FileSizeFormatter.humanize(1023 * 1024)).toBe('1023.00 Kb');
+    });
+
+    it('should format 1 MB', () => {
+        expect(FileSizeFormatter.humanize(1024 * 1024)).toBe('1.00 Mb');
+    });
+
+    it('should format 1023 MB', () => {
+        expect(FileSizeFormatter.humanize(1023 * 1024 * 1024)).toBe('1023.00 Mb');
+    });
+
+    it('should format 1 GB', () => {
+        expect(FileSizeFormatter.humanize(1024 * 1024 * 1024)).toBe('1.00 Gb');
+    });
+
+    it('should format 1023 GB', () => {
+        expect(FileSizeFormatter.humanize(1023 * 1024 * 1024 * 1024)).toBe('1023.00 Gb');
+    });
+
+    it('should format 1 TB', () => {
+        expect(FileSizeFormatter.humanize(1024 * 1024 * 1024 * 1024)).toBe('1.00 Tb');
+    });
+
+    it('should format 1023 TB', () => {
+        expect(FileSizeFormatter.humanize(1023 * 1024 * 1024 * 1024 * 1024)).toBe('1023.00 Tb');
+    });
+
+    it('should format 1024 TB', () => {
+        expect(FileSizeFormatter.humanize(1024 * 1024 * 1024 * 1024 * 1024)).toBe('1024.00 Tb');
+    });
+});
