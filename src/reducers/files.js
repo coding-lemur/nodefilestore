1bdd30474024fe935514b3a448c0133ccf5b566b
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2016-11-06 15:21:44 +0100

    optimized code

--- src/reducers/files.js
+++ src/reducers/files.js
@@ -3,7 +3,10 @@ import * as actionCreators from '../actions/action-creators';
 export default function files(state = [], action) {
     switch (action.type) {
         case actionCreators.ADD_FILES:
-            return state.concat(action.files);
+            return [
+                ...state,
+                action.files
+            ];
         case actionCreators.DELETE_FILE:
             const indexToRemove = state.indexOf(action.file);
             return [
